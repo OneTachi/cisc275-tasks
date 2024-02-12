ee4271ebfe7df7a01d6259758b260f4cdc1568cb
sidd <n2verz@gmail.com>
2024-02-11 19:22:08 -0500

    task 3 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript. Hello World!
+                Siddharth Lokula
             </header>
-            <header className="App-header">Siddharth Lokula</header>
+            <h2 className="header">OMG, a header but smaller?!</h2>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <span style={{ color: "red" }}>colored text</span>
+                        <p>Pets I think are super cool:</p>
+                        <ul>
+                            <li>Komodo Dragon</li>
+                            <li>Emu</li>
+                            <li>Mantis</li>
+                        </ul>
+                        <p style={{ fontSize: "32px" }}>This gonna be BIG</p>
+                        <div className="rectangle"></div>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                        <div className="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
+            <img
+                src={require("./images/guinea.jpg")}
+                style={{ height: "50%", width: "50%" }}
+                alt="A little guinea pig!"
+            />
         </div>
     );
 }
