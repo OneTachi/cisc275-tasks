3472f1494c566aefe6bd56d1466aa5abb61f17a7
sidd <n2verz@gmail.com>
2024-02-20 08:45:50 -0500

    add return to map in triple

--- src/arrays.ts
+++ src/arrays.ts
@@ -17,7 +17,9 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    const tripled: number[] = [...numbers].map((num: number) => num * 3);
+    const tripled: number[] = [...numbers].map(
+        (num: number): number => num * 3
+    );
     return tripled;
 }
 
