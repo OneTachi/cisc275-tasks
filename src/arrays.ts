b3385f5fcc92f24c64300d2368993216dd8e9a17
sidd <n2verz@gmail.com>
2024-02-20 08:53:54 -0500

    add map to str->int

--- src/arrays.ts
+++ src/arrays.ts
@@ -28,7 +28,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const converted_num: number[] = [...numbers].map((num: string): number =>
+        Number.isNaN(parseInt(num)) ? 0 : parseInt(num)
+    );
+    return converted_num;
 }
 
 /**
