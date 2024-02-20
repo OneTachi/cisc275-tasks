ff56cf3558cf9d8266dd500a6cfb1fde8781a4e3
sidd <n2verz@gmail.com>
2024-02-20 08:42:59 -0500

    saved map for pre commit

--- src/arrays.ts
+++ src/arrays.ts
@@ -17,7 +17,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled: number[] = [...numbers].map((num: number) => num * 3);
+    return tripled;
 }
 
 /**
