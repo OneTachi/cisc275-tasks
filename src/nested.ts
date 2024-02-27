230c1b0a356e217743bf7046638edeec85d07bab
sidd <n2verz@gmail.com>
2024-02-27 18:25:39 -0500

    format csv

--- src/nested.ts
+++ src/nested.ts
@@ -108,7 +108,20 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const csv: string[] = [...questions].map((question: Question): string =>
+        (
+            question.id +
+            "," +
+            question.name +
+            "," +
+            question.options.length +
+            "," +
+            question.points +
+            "," +
+            question.published
+        ).toString()
+    );
+    return "id,name,options,points,published\n" + csv.join("\n");
 }
 
 /**
