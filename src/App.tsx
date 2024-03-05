1dfb2f1d8520468b7086c17c8cbd1bf1156e4ab3
sidd <n2verz@gmail.com>
2024-03-05 09:53:33 -0500

    add visible state to reveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -18,39 +14,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript. Hello World!
                 Siddharth Lokula
             </header>
-<<<<<<< HEAD
-            <h2 className="header">OMG, a header but smaller?!</h2>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <Container>
-                <Row>
-                    <Col>
-                        <span style={{ color: "red" }}>colored text</span>
-                        <p>Pets I think are super cool:</p>
-                        <ul>
-                            <li>Komodo Dragon</li>
-                            <li>Emu</li>
-                            <li>Mantis</li>
-                        </ul>
-                        <p style={{ fontSize: "32px" }}>This gonna be BIG</p>
-                        <div className="rectangle"></div>
-                    </Col>
-                    <Col>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                        <div className="rectangle"></div>
-                    </Col>
-                </Row>
-            </Container>
-            <img
-                src={require("./images/guinea.jpg")}
-                style={{ height: "50%", width: "50%" }}
-                alt="A little guinea pig!"
-            />
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -63,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
