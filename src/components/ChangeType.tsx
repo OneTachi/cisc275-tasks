b3e17d50ebbb5863340c7c3fa3efdbf386fcf7b9
sidd <n2verz@gmail.com>
2024-03-05 10:13:24 -0500

    formatting & start button

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,20 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [answer, setAnswer] = useState<QuestionType>("short_answer_question");
+    const rec: Record<QuestionType, QuestionType> = {
+        short_answer_question: "multiple_choice_question",
+        multiple_choice_question: "short_answer_question"
+    };
+    const text_rec: Record<QuestionType, string> = {
+        short_answer_question: "Short Answer",
+        multiple_choice_question: "Multiple Choice"
+    };
+
+    return (
+        <div>
+            <Button onClick={() => setAnswer(rec[answer])}>Change Type</Button>
+            {text_rec[answer]}
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,20 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                onClick={(): void => {
+                    setProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+                disabled={inProgress}
+            >
+                Start Quiz
+            </Button>
+        </div>
+    );
 }
