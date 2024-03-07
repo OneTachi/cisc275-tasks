fe96211a6aa88755b05a19223c9420621b8ae4f4
sidd <n2verz@gmail.com>
2024-03-07 11:54:46 -0500

    setup buttons and request attempt form

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [requestAttempts, setRequestAttempts] = useState<number>(3);
     return (
         <div>
             <h3>Give Attempts</h3>
+            <p>Attempts Left: {attempts}</p>
+            <Form.Group controlId="requestAttempts">
+                <Form.Control
+                    type="number"
+                    value={requestAttempts}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setRequestAttempts(
+                            parseInt(event.target.value) || requestAttempts
+                        )
+                    }
+                    placeholder="Enter Wanted Requests"
+                />
+            </Form.Group>
+            <Button
+                onClick={() => setAttempts(attempts - 1)}
+                disabled={attempts === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttempts(Math.min(attempts + 1, requestAttempts))
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
