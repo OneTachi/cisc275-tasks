9a252ce0ca9c75667b530b56966784a1b3bad53b
sidd <n2verz@gmail.com>
2024-03-07 11:28:16 -0500

    create short-answer form to check answer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
     return (
         <div>
+            <Form.Group controlId="answerQuestion">
+                <Form.Label>Enter your answer here!</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Answer"
+                    value={answer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            {answer === expectedAnswer ? <div>✔️</div> : <div>❌</div>}
             <h3>Check Answer</h3>
         </div>
     );
