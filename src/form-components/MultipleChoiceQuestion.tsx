a1ec80e885cec11d5865cc8eebe9fc093c957c82
sidd <n2verz@gmail.com>
2024-03-07 19:18:11 -0500

    Create select form and map options

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,6 @@
+import exp from "constants";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +9,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+    const copied_options = [...options];
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Select
+                value={answer}
+                onChange={(event: React.ChangeEvent<HTMLSelectElement>): void =>
+                    setAnswer(event.target.value)
+                }
+            >
+                {copied_options.map((option: string) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </Form.Select>
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
